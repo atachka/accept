bc484aa3ca5b00d54b733a5ce685e34ee5b42ff6
atachka <56038697+atachka@users.noreply.github.com>
2022-12-12 15:59:04 +0400

    initial commit

---
+++ components/header/header.js
@@ -0,0 +1,9 @@
+import React from "react";
+import styles from "./header.module.css";
+export default function Header() {
+  return (
+    <header className={styles.header__container}>
+      <div></div>
+    </header>
+  );
+}
---
+++ components/map/map.js
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import ReactMapGL, { Marker } from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+
+export {};
+
+export default function Map() {
+  const [viewport, setViewport] = useState({
+    latitude: 41.711747,
+    longitude: 44.774907,
+    zoom: 15,
+  });
+  return (
+    <div style={{ width: "300px", height: "300px" }}>
+      <ReactMapGL
+        {...viewport}
+        style={{ width: "684px", height: "380px" }}
+        mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_API_TOKEN}
+        onMove={(nextViewport) => setViewport(nextViewport.viewState)}
+        mapStyle="mapbox://styles/ata123/claausf30002514phgizd0nb0"
+      >
+        <Marker
+          latitude={41.711747}
+          longitude={44.774907}
+          anchor="bottom"
+        ></Marker>
+
+        {/* <div
+          onClick={() => {
+            setViewport({
+              ...viewport,
+              zoom: 0,
+              viewState: {
+                ...viewport.viewState,
+                zoom: viewport.viewState?.zoom
+                  ? viewport.viewState?.zoom - 1
+                  : viewport.zoom - 1,
+              },
+            });
+          }}
+          style={{
+            position: "absolute",
+            width: "100px",
+            height: "100px",
+            backgroundColor: "green",
+          }}
+        >
+          avoie
+        </div> */}
+      </ReactMapGL>
+    </div>
+  );
+}
